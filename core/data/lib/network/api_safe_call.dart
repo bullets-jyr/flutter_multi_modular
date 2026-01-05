e5b57ff42cd1e98913f69a139436fd000c169943
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-05 17:17:14 +0900

    feat: Implement advanced error management

--- core/data/lib/network/api_safe_call.dart
+++ core/data/lib/network/api_safe_call.dart
@@ -1,4 +1,7 @@
 import 'package:dartz/dartz.dart';
+import 'package:data/error_handler/data_source.dart';
+import 'package:data/error_handler/data_source_extension.dart';
+import 'package:data/error_handler/dio_error_handler.dart';
 import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:domain/model/localized_message.dart';
@@ -14,6 +17,30 @@ import 'package:domain/model/localized_message.dart';
 //   }
 // }
 
+// Future<Either<Failure, T>> safeAPiCall<T>(
+//   NetworkInfo networkInfo,
+//   Future<T> Function() apiCall,
+// ) async {
+//   if (await networkInfo.isConnected) {
+//     try {
+//       final response = await apiCall();
+//       return Right(response);
+//     } catch (error) {
+//       return Left(
+//         Failure(0, LocalizedMessage("", "")),
+//       ); // todo add error handler here
+//     }
+//   } else {
+//     // no internet connection
+//     return Left(
+//       Failure(
+//         -500,
+//         LocalizedMessage("network connectivity issue, please check", ""),
+//       ),
+//     ); // todo add error handler here
+//   }
+// }
+
 Future<Either<Failure, T>> safeAPiCall<T>(
   NetworkInfo networkInfo,
   Future<T> Function() apiCall,
@@ -23,17 +50,10 @@ Future<Either<Failure, T>> safeAPiCall<T>(
       final response = await apiCall();
       return Right(response);
     } catch (error) {
-      return Left(
-        Failure(0, LocalizedMessage("", "")),
-      ); // todo add error handler here
+      return Left(ErrorHandler.handle(error).failure);
     }
   } else {
     // no internet connection
-    return Left(
-      Failure(
-        -500,
-        LocalizedMessage("network connectivity issue, please check", ""),
-      ),
-    ); // todo add error handler here
+    return Left(DataSource.noInternetConnection.getFailure());
   }
 }
--- core/domain/lib/model/failure.dart
+++ core/domain/lib/model/failure.dart
@@ -3,7 +3,7 @@ import 'package:domain/model/localized_message.dart';
 // error model that we expect to receive from API
 class Failure {
   int code; // 200,201, 303, 404, 400, 500, 503 ...etc
-  LocalizedMessage message;
+  String message;
 
   Failure(this.code, this.message);
 }

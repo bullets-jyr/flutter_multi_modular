f244c7984fadba92e5daa295c6bce1889072ea4e
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-05 15:08:47 +0900

    feat: Add login module blueprint

---
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:extensions/constants/constants.dart';
+import 'package:extensions/extensions.dart';
+import 'package:login/data/response/login_response.dart';
+import 'package:login/domain/model/login_model.dart';
+
+// extension LoginResponseMapper on LoginResponse? {
+//   LoginModel toDomain() {
+//     return LoginModel(name: this?.name, phone: this?.phone, age: this?.age);
+//   }
+// }
+
+extension LoginResponseMapper on LoginResponse? {
+  LoginModel toDomain() {
+    return LoginModel(
+      name: this?.name.orEmpty() ?? Constants.empty,
+      phone: this?.phone.orEmpty() ?? Constants.empty,
+      age: this?.age.orZero() ?? Constants.zero,
+    );
+  }
+}
---
+++ features/auth/login/lib/data/service/login_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:login/data/response/login_response.dart';
+import 'package:retrofit/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+
+part 'login_service.g.dart';
+
+const String baseUrl = "https://minafarid.mocklab.io";
+
+@RestApi(baseUrl: baseUrl)
+abstract class LoginService {
+  factory LoginService(
+    Dio dio, {
+    String baseUrl,
+    ParseErrorLogger errorLogger,
+  }) = _LoginService;
+
+  @POST("/customers/login")
+  Future<HttpResponse<LoginResponse>> login(
+    @Field("email") String email,
+    @Field("password") String password,
+  );
+}

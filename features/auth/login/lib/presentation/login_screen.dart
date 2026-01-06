9fa8358a8ec447ca8bb56cafecc655296e6e520f
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-06 12:38:00 +0900

    feat: Design login screen in presentation layer

---
+++ features/auth/login/lib/presentation/login_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_multi_modular/di/injection.dart';
+import 'package:login/domain/use_case/login_use_case.dart';
+import 'package:login/presentation/login_bloc.dart';
+import 'package:login/presentation/login_event.dart';
+import 'package:login/presentation/login_state.dart';
+
+class LoginScreen extends StatelessWidget {
+  final loginUseCase = getIt<LoginUseCase>();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login")),
+      body: BlocProvider(
+        create: (context) => LoginBloc(loginUseCase),
+        child: BlocBuilder<LoginBloc, LoginState>(
+          builder: (context, state) {
+            return Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: usernameController,
+                    onChanged: (value) {
+                      context.read<LoginBloc>().add(UsernameChanged(value));
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Username",
+                      errorText: state is LoginInvalid
+                          ? state.usernameError
+                          : null,
+                    ),
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    onChanged: (value) {
+                      context.read<LoginBloc>().add(PasswordChanged(value));
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      errorText: state is LoginInvalid
+                          ? state.passwordError
+                          : null,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      final username = usernameController.text;
+                      final password = passwordController.text;
+                      context.read<LoginBloc>().add(
+                        LoginButtonPressed(username, password),
+                      );
+                    },
+                    child: Text("Login"),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:app_settings/provider/app_settings_provider.dart';
-import 'package:datastore/provider/session/session_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_modular/di/injection.dart';
 import 'package:injectable/injectable.dart';
-import 'package:login/login.dart';
+import 'package:login/presentation/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,63 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    doLogin();
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final appSettingsProvider = getIt<AppSettingsProvider>();
-    final sessionProvider = getIt<SessionProvider>();
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            Text(
-              'App Language: ${appSettingsProvider.getAppLanguage()}',
-            ),
-            Text(
-              'App Theme: ${appSettingsProvider.getThemeType()}',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'Session ClientId: ${sessionProvider.getClientId()}',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: LoginScreen(),
     );
   }
 }

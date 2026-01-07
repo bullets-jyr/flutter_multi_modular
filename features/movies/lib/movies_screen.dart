00df4b6d96c1a2df5472d1e3dd45bf69560d7797
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-07 17:06:40 +0900

    feat: Manage Movies & Settings tabs with BLoC

---
+++ features/movies/lib/movies_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MoviesScreen extends StatelessWidget {
+  const MoviesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Movies will be displayed here"),
+      ),
+    );
+  }
+}
